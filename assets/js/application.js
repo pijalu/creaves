045eaca43ee76dc4c4b88e5b923c31041675e9fe
Pierre Poissinger <pierre.poissinger@gmail.com>
2021-02-21 14:31:06 +0100

    Moved more items out to CDN

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,5 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
-require("bootstrap/dist/js/bootstrap.bundle.js");
+//require("bootstrap/dist/js/bootstrap.bundle.js");
 //require("@fortawesome/fontawesome-free/js/all.js");
 
 $(() => {
