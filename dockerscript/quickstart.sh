6e7d73a28f9b30b11d8bc4e8cfdacad89cef0a61
Pierre Poissinger <pierre.poissinger@gmail.com>
2021-03-13 18:52:27 +0100

    Use mysql

--- dockerscript/quickstart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Quick start creaves (dev)"
-unset GO_ENV
-/bin/app migrate && \
-    /bin/app task db:seed && \
-    /bin/app
--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,110 +1,252 @@
-CREATE TABLE IF NOT EXISTS "schema_migration" (
-"version" TEXT NOT NULL
-);
-CREATE UNIQUE INDEX "schema_migration_version_idx" ON "schema_migration" (version);
-CREATE TABLE IF NOT EXISTS "users" (
-"id" TEXT PRIMARY KEY,
-"email" TEXT NOT NULL,
-"admin" bool NOT NULL DEFAULT 'false',
-"approved" bool NOT NULL DEFAULT 'false',
-"password_hash" TEXT NOT NULL,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "logentries" (
-"id" TEXT PRIMARY KEY,
-"user_id" char(36) NOT NULL,
-"description" TEXT NOT NULL,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL,
-FOREIGN KEY (user_id) REFERENCES users (id)
-);
-CREATE TABLE IF NOT EXISTS "discoverers" (
-"id" TEXT PRIMARY KEY,
-"firstname" TEXT NOT NULL,
-"lastname" TEXT NOT NULL,
-"address" TEXT NOT NULL,
-"city" TEXT NOT NULL,
-"country" TEXT NOT NULL,
-"email" TEXT,
-"phone" TEXT,
-"note" TEXT,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "discoveries" (
-"id" TEXT PRIMARY KEY,
-"location" TEXT NOT NULL,
-"date" DATETIME NOT NULL,
-"reason" TEXT,
-"note" TEXT,
-"discoverer_id" char(36) NOT NULL,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL,
-FOREIGN KEY (discoverer_id) REFERENCES discoverers (id) ON DELETE CASCADE
-);
-CREATE TABLE IF NOT EXISTS "animaltypes" (
-"id" TEXT PRIMARY KEY,
-"name" TEXT NOT NULL,
-"description" TEXT,
-"def" bool NOT NULL DEFAULT 'false',
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "intakes" (
-"id" TEXT PRIMARY KEY,
-"date" DATETIME NOT NULL,
-"general" TEXT NOT NULL,
-"wounds" TEXT,
-"parasites" TEXT,
-"remarks" TEXT,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "outtaketypes" (
-"id" TEXT PRIMARY KEY,
-"name" TEXT NOT NULL,
-"description" TEXT,
-"def" bool NOT NULL DEFAULT 'false',
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL
-);
-CREATE TABLE IF NOT EXISTS "outtakes" (
-"id" TEXT PRIMARY KEY,
-"date" DATETIME NOT NULL,
-"outtaketype_id" char(36) NOT NULL,
-"location" TEXT,
-"note" TEXT,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL,
-FOREIGN KEY (outtaketype_id) REFERENCES outtaketypes (id)
-);
-CREATE TABLE IF NOT EXISTS "animals" (
-"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-"species" TEXT NOT NULL,
-"ring" TEXT,
-"cage" TEXT,
-"animalage_id" char(36) NOT NULL,
-"animaltype_id" char(36) NOT NULL,
-"discovery_id" char(36) NOT NULL,
-"intake_id" char(36) NOT NULL,
-"outtake_id" char(36),
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL,
-FOREIGN KEY (animaltype_id) REFERENCES animaltypes (id),
-FOREIGN KEY (discovery_id) REFERENCES discoveries (id),
-FOREIGN KEY (intake_id) REFERENCES intakes (id),
-FOREIGN KEY (outtake_id) REFERENCES outtakes (id)
-);
-CREATE TABLE sqlite_sequence(name,seq);
-CREATE UNIQUE INDEX "users_email_idx" ON "users" (email);
-CREATE UNIQUE INDEX "animaltypes_name_idx" ON "animaltypes" (name);
-CREATE UNIQUE INDEX "outtaketypes_name_idx" ON "outtaketypes" (name);
-CREATE TABLE IF NOT EXISTS "animalages" (
-"id" TEXT PRIMARY KEY,
-"name" TEXT NOT NULL,
-"description" TEXT,
-"def" bool NOT NULL,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL
-);
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: creaves
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `animalages`
+--
+
+DROP TABLE IF EXISTS `animalages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animalages` (
+  `id` char(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `def` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `animals`
+--
+
+DROP TABLE IF EXISTS `animals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animals` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `species` varchar(255) NOT NULL,
+  `ring` varchar(255) DEFAULT NULL,
+  `cage` varchar(255) DEFAULT NULL,
+  `animalage_id` char(36) NOT NULL,
+  `animaltype_id` char(36) NOT NULL,
+  `discovery_id` char(36) NOT NULL,
+  `intake_id` char(36) NOT NULL,
+  `outtake_id` char(36) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `animaltype_id` (`animaltype_id`),
+  KEY `discovery_id` (`discovery_id`),
+  KEY `intake_id` (`intake_id`),
+  KEY `outtake_id` (`outtake_id`),
+  CONSTRAINT `animals_ibfk_1` FOREIGN KEY (`animaltype_id`) REFERENCES `animaltypes` (`id`),
+  CONSTRAINT `animals_ibfk_2` FOREIGN KEY (`discovery_id`) REFERENCES `discoveries` (`id`),
+  CONSTRAINT `animals_ibfk_3` FOREIGN KEY (`intake_id`) REFERENCES `intakes` (`id`),
+  CONSTRAINT `animals_ibfk_4` FOREIGN KEY (`outtake_id`) REFERENCES `outtakes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `animaltypes`
+--
+
+DROP TABLE IF EXISTS `animaltypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `animaltypes` (
+  `id` char(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `def` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `animaltypes_name_idx` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `discoverers`
+--
+
+DROP TABLE IF EXISTS `discoverers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `discoverers` (
+  `id` char(36) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `note` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `discoveries`
+--
+
+DROP TABLE IF EXISTS `discoveries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `discoveries` (
+  `id` char(36) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `reason` text,
+  `note` text,
+  `discoverer_id` char(36) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `discoverer_id` (`discoverer_id`),
+  CONSTRAINT `discoveries_ibfk_1` FOREIGN KEY (`discoverer_id`) REFERENCES `discoverers` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `intakes`
+--
+
+DROP TABLE IF EXISTS `intakes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `intakes` (
+  `id` char(36) NOT NULL,
+  `date` datetime NOT NULL,
+  `general` text NOT NULL,
+  `wounds` text,
+  `parasites` text,
+  `remarks` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `logentries`
+--
+
+DROP TABLE IF EXISTS `logentries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logentries` (
+  `id` char(36) NOT NULL,
+  `user_id` char(36) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `logentries_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `outtakes`
+--
+
+DROP TABLE IF EXISTS `outtakes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `outtakes` (
+  `id` char(36) NOT NULL,
+  `date` datetime NOT NULL,
+  `outtaketype_id` char(36) NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `note` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `outtaketype_id` (`outtaketype_id`),
+  CONSTRAINT `outtakes_ibfk_1` FOREIGN KEY (`outtaketype_id`) REFERENCES `outtaketypes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `outtaketypes`
+--
+
+DROP TABLE IF EXISTS `outtaketypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `outtaketypes` (
+  `id` char(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `def` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `outtaketypes_name_idx` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `schema_migration`
+--
+
+DROP TABLE IF EXISTS `schema_migration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `schema_migration` (
+  `version` varchar(14) NOT NULL,
+  UNIQUE KEY `schema_migration_version_idx` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` char(36) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `approved` tinyint(1) NOT NULL DEFAULT '0',
+  `password_hash` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_idx` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-13 18:35:03
