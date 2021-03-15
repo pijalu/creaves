e15667c096d9da561fc0168cfe38843c914c019d
Pierre Poissinger <pierre.poissinger@gmail.com>
2021-03-15 22:05:01 +0100

    Fix #1: Add outtaken style and apply to animal list

--- actions/animals.go
+++ actions/animals.go
@@ -60,7 +60,7 @@ func (v AnimalsResource) List(c buffalo.Context) error {
 	q := tx.PaginateFromParams(c.Params())
 
 	// Retrieve all Animals from the DB
-	if err := q.Eager().Order("updated_at desc").All(animals); err != nil {
+	if err := q.Eager().Order("created_at desc").All(animals); err != nil {
 		return err
 	}
 
