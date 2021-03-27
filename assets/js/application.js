897ee84fb450373b83efdddc6112df6285a9a629
Pierre Poissinger <pierre.poissinger@gmail.com>
2021-03-27 22:03:53 +0100

    Revert "Move all dep to local"
    This reverts commit f5d7277c6ca95493549de67850102603ebe926e3.

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,10 +1,8 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("./wizard.js");
 require("./jquery.auto-complete.js");
-require("bootstrap/dist/js/bootstrap.bundle.js");
-require("@fortawesome/fontawesome-free/js/all.js");
-require("jquery-datetimepicker/build/jquery.datetimepicker.full.js");
-
+//require("bootstrap/dist/js/bootstrap.bundle.js");
+//require("@fortawesome/fontawesome-free/js/all.js");
 
 $(() => {
     
