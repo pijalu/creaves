4bc25aa8f5171af32771d2925906df11ba844732
Pierre Poissinger <pierre.poissinger@gmail.com>
2021-03-28 10:12:46 +0200

    Move all dep to local (reredux) and changed calendar

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,8 +1,9 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("./wizard.js");
 require("./jquery.auto-complete.js");
-//require("bootstrap/dist/js/bootstrap.bundle.js");
-//require("@fortawesome/fontawesome-free/js/all.js");
+require("bootstrap/dist/js/bootstrap.bundle.js");
+require("@fortawesome/fontawesome-free/js/all.js");
+require("flatpickr");
 
 $(() => {
     
