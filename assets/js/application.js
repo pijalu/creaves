1e1a663c918f022d6004fa74408ebc4bf3a62ac9
Pierre Poissinger <pierre.poissinger@gmail.com>
2022-12-27 22:54:03 +0100

    Use bootstrap table for landing

--- assets/js/application.js
+++ assets/js/application.js
@@ -6,6 +6,9 @@ require("bootstrap/dist/js/bootstrap.bundle.js");
 require("@fortawesome/fontawesome-free/js/all.js");
 require("flatpickr");
 require('jquery-ujs');
+require('bootstrap-table/dist/bootstrap-table.js');
+require('bootstrap-table/dist/bootstrap-table-locale-all.js');
+
 
 $(() => {
     
