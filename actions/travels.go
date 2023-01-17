39f62f8b4a05cf2e2be1ed7b2970f2429cdded57
Pierre Poissinger <pierre.poissinger@gmail.com>
2023-01-17 20:38:01 +0100

    Fix the travel animal year number

--- actions/travels.go
+++ actions/travels.go
@@ -129,8 +129,8 @@ func (v TravelsResource) New(c buffalo.Context) error {
 		}
 	}
 
-	animalID := c.Param("animal_id")
-	if len(animalID) > 0 {
+	animalYearNumber := c.Param("animal_year_number")
+	if len(animalYearNumber) > 0 {
 		// Get the DB connection from the context
 		tx, ok := c.Value("tx").(*pop.Connection)
 		if !ok {
@@ -142,17 +142,33 @@ func (v TravelsResource) New(c buffalo.Context) error {
 
 		// for message
 		data := map[string]interface{}{
-			"animalID": animalID,
+			"animalID": animalYearNumber,
 		}
 
-		if err := tx.Find(animal, animalID); err != nil {
+		c.Logger().Debug("animalYearNumber:", animalYearNumber)
+		matches := AnimalYearNumberRegEx.FindStringSubmatch(animalYearNumber)
+		if matches == nil {
+			return fmt.Errorf("invalid year number: %s", animalYearNumber)
+		}
+		c.Logger().Debug("animalYearNumber regex matches:", matches)
+		q := tx.Where("yearNumber = ?", matches[1])
+		if len(matches) == 4 && len(matches[3]) == 2 {
+			q = q.Where("year = ?", fmt.Sprintf("20%s", matches[3]))
+		}
+
+		err := q.Order("ID desc").Eager().First(animal)
+		if err != nil {
+			c.Logger().Debug("Error:", err)
 			c.Flash().Add("danger", T.Translate(c, "travels.animal.not.found", data))
 			errCode = http.StatusNotFound
 		}
 
 		if errCode != http.StatusOK {
-			return c.Render(errCode, r.HTML("/outtakes/new.plush.html"))
+			return c.Render(errCode, r.HTML("/travels/new.plush.html"))
 		}
+
+		c.Logger().Debugf("Loaded animal %v", animal)
+
 		travel.Animal = animal
 		travel.AnimalID = animal.ID
 		c.Set("animal", animal)
