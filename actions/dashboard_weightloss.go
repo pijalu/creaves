27a3758801e1843adcbc7071e5fd6bf6649766c6
Pierre Poissinger <pierre.poissinger@gmail.com>
2024-12-23 12:21:00 +0100

    Fix #180

--- actions/dashboard_weightloss.go
+++ actions/dashboard_weightloss.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop/v6"
+	"github.com/gofrs/uuid"
 )
 
 const ANIMALS_WITH_WEIGTHLOSS = `
@@ -13,7 +14,7 @@ WITH ParsedWeights AS (
     SELECT
         animal_id,
         date,
-        CAST(NULLIF(weight, '') AS DECIMAL(10,2)) AS weight_in_grams
+        CAST(NULLIF(weight, '') AS DECIMAL(10,0)) AS weight_in_grams
     FROM cares 
     WHERE date >= DATE_SUB(CURDATE(), INTERVAL 7 day) -- Filter last 7 days
       AND weight is not null and weight <> "" and (CAST(NULLIF(weight, '') AS DECIMAL(10,2))) is not null
@@ -26,26 +27,34 @@ RankedWeights AS (
 		ROW_NUMBER() OVER (PARTITION BY animal_id ORDER BY date DESC) AS recent_rank,
         ROW_NUMBER() OVER (PARTITION BY animal_id ORDER BY date ASC) AS oldest_rank
     FROM ParsedWeights
-)
-SELECT 
-	a.*
-	-- a.year, a.yearNumber, a.cage, -- t1.*, t2.*
-    -- t1.animal_id, t1.date, t1.weight_in_grams, t2.date, t2.weight_in_grams, t1.weight_in_grams - t2.weight_in_grams
-FROM RankedWeights t1
-JOIN RankedWeights t2
-    ON t1.animal_id = t2.animal_id
-JOIN animals a
-	ON t1.animal_id = a.ID
-WHERE t1.recent_rank = 1  -- Most recent weight
-  AND t2.oldest_rank = 1  -- oldest recent weight
-  AND t1.weight_in_grams IS NOT NULL
-  AND t2.weight_in_grams IS NOT NULL
-  AND t1.weight_in_grams <= t2.weight_in_grams * 0.9 -- Weight decreased by 10%
-  AND a.outtake_id is null
+),
+AnimalsInNeed AS (		
+	SELECT a.*
+	FROM RankedWeights t1
+	JOIN RankedWeights t2
+		ON t1.animal_id = t2.animal_id
+	JOIN animals a
+		ON t1.animal_id = a.ID
+	WHERE t1.recent_rank = 1  -- Most recent weight
+	  AND t2.oldest_rank = 1  -- oldest recent weight
+	  AND t1.weight_in_grams IS NOT NULL
+	  AND t2.weight_in_grams IS NOT NULL
+	  AND t1.weight_in_grams < t2.weight_in_grams * 0.9 -- Weight decreased by 10%
+	  AND a.outtake_id is null)
+SELECT a.*, group_concat(rw.weight_in_grams ORDER by rw.oldest_rank asc SEPARATOR '=>') AS 'weights'
+	FROM AnimalsInNeed a
+    JOIN RankedWeights rw
+      ON a.ID = rw.animal_id
+GROUP BY a.ID
 `
 
-func listAnimalWithWeightLoss(c buffalo.Context) (*models.Animals, error) {
-	animals := &models.Animals{}
+type AnimalWithWeight struct {
+	models.Animal
+	Weights string `json:"weights" db:"weights"`
+}
+
+func listAnimalWithWeightLoss(c buffalo.Context) (*[]AnimalWithWeight, error) {
+	a := &[]AnimalWithWeight{}
 
 	tx, ok := c.Value("tx").(*pop.Connection)
 	if !ok {
@@ -53,8 +62,25 @@ func listAnimalWithWeightLoss(c buffalo.Context) (*models.Animals, error) {
 	}
 
 	// Retrieve all animals with today treatments from the DB
-	if err := tx.RawQuery(ANIMALS_WITH_WEIGTHLOSS).All(animals); err != nil {
+	if err := tx.RawQuery(ANIMALS_WITH_WEIGTHLOSS).All(a); err != nil {
 		return nil, err
 	}
-	return EnrichAnimals(animals, c)
+
+	// Populate ages
+	if len(*a) > 0 {
+		// Ages
+		agsMap := make(map[uuid.UUID]models.Animalage)
+		if ags, err := animalages(c); err != nil {
+			return nil, err
+		} else {
+			for _, a := range *ags {
+				agsMap[a.ID] = a
+			}
+		}
+		for i := 0; i < len(*a); i++ {
+			(*a)[i].Animalage = agsMap[(*a)[i].AnimalageID]
+		}
+	}
+
+	return a, nil
 }
